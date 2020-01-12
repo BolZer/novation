b3caf1782223c430d2a7046a4baac911cd948236
BolZer <noehles@easybill.de>
2020-01-12 11:51:58 +0100

    Styling of the Quadrants

--- lib/widgets/TextPlaceholder.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:flutter/material.dart';
-
-class TextPlaceholder extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text("Placeholder");
-  }
-}
