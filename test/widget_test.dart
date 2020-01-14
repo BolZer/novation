7e6fbd34455c61a1f7be1c7b0052bbcb3d70872d
J. N <noehles@easybill.de>
2020-01-14 11:29:03 +0100

    Initialize Project

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,37 +1,42 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_sound_board/bloc.dart';
-import 'package:flutter_sound_board/event.dart';
 import 'package:flutter_sound_board/state.dart';
 import 'package:flutter_sound_board/widgets/SoundBoard.dart';
 import 'package:flutter_sound_board/widgets/SoundBoardToolbar.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-class MockSoundBoarBloc extends MockBloc<SoundBoardEvent, SoundBoardState> implements SoundBoardBloc {}
 
 void main() {
   group('Testing Soundboard Screen', () {
     SoundBoardBloc soundBoardBloc;
 
     setUp(() {
-      soundBoardBloc = MockSoundBoarBloc();
+      soundBoardBloc = SoundBoardBloc();
     });
 
     tearDown(() {
       soundBoardBloc?.close();
     });
 
+    test('initial state is correct', (){
+      expect(soundBoardBloc.initialState, Ready(page: 1, isInEditMode: false, sounds: [], focusedSoundButton: null));
+    });
+
     testWidgets('Test the Main Widget to display tiles', (WidgetTester tester) async {
+      
+      whenListen(soundBoardBloc,  Stream.value(Ready(page: 1, isInEditMode: false, sounds: [], focusedSoundButton: null, audioPlayer: AudioPlayer())));
+
       await tester.pumpWidget(BlocProvider(
         create: (BuildContext context) => soundBoardBloc,
-        child: MaterialApp(
-          title: 'SB',
-          theme: ThemeData.dark(),
-          home: SoundBoard(),
-        ),
+        child: new MediaQuery(
+            data: new MediaQueryData(),
+            child: new MaterialApp(home: SoundBoard())
+        )
       ));
+
       expect(find.byType(SoundBoardToolbar), findsOneWidget);
     });
   });
